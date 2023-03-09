d40fb26146cfb7d636b6796de42a9e9dae2c71bd
hetsonii <21cs068@charusat.edu.in>
2023-03-09 18:18:20 +0530

    Day3 Complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Het App"),
+        title: Text("Catalog App"),
       ),
       body: Center(
         child: Container(
